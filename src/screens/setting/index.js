32e255c0e2c33a255228beabd935c90acf821ddd
aml3mohammed <aml.mohammrd@gmail.com>
2020-03-30 18:33:30 +0200

    setting screen design

--- src/screens/setting/index.js
+++ src/screens/setting/index.js
@@ -1,41 +1,37 @@
-import React, { Component } from 'react'
-import Image from "assets/images/logo.png"
-import ButtonData from './json';
-import classes from './style.less'
+import React, { Component } from "react";
+import Image from "assets/images/logo.png";
+import ButtonData from "./json";
+import classes from "./style.less";
 
 class Start extends Component {
- 
- handelstart =()=>{
+  handelstart = () => {
     const { history } = this.props;
-    history.push('/home')
-}
-renderButon = ()=>{
-    return ButtonData.map((d,v)=>{
-        return(
-            <button>
-                {d.title}
-                <img src={d.icon}/>
-            </button>
-        )
-    })
-}
+    history.push("/home");
+  };
+  renderButon = () => {
+    return ButtonData.map((d, v) => {
+      return (
+        <button className={classes.settingButton}>
+          <div>{d.title}</div>
+          <img src={d.icon} className={classes.icon} />
+        </button>
+      );
+    });
+  };
 
-    render() {
-        return (
-        <div className={classes.body}>
-            <div className={classes.flexContainer}>
-                <div className={classes.flexbox}>
-                {this.renderButon()}
-                </div>
-            </div>
-            <p>Select Language</p>
-            <div>
-            <button>English</button>
-            <button>العربيه</button>
-            </div>
+  render() {
+    return (
+      <div className={classes.body}>
+        <div className={classes.flexContainer}>
+          <div className={classes.flexbox}>{this.renderButon()}</div>
         </div>
-        )
-    }
+        <p className={classes.selectLanguage}>Select Language</p>
+        <div className={classes.selectFlexbox}>
+          <button className={classes.english}>English</button>
+          <button className={classes.arabic}>العربية</button>
+        </div>
+      </div>
+    );
+  }
 }
 export default Start;
-
