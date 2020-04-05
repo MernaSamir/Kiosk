24754b9d3b22890266329f689db6f3c1b7bbca89
aml3mohammed <aml.mohammrd@gmail.com>
2020-04-05 11:53:22 +0200

    body design

--- src/screens/order/body/index.js
+++ src/screens/order/body/index.js
@@ -1,68 +1,65 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import mapDispatchToProps from 'helpers/actions/main'
-import {get,min} from 'lodash';
-import applyFilters from 'helpers/functions/filters';
-import Table from '../../../assets/images/003-serving-dish@3x.png';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapDispatchToProps from "helpers/actions/main";
+import { get, min } from "lodash";
+import applyFilters from "helpers/functions/filters";
+import Table from "../../../assets/images/eatIn.png";
+import classes from "./style.less";
 
 class MainItems extends Component {
- 
-
-    renderItems=()=>{
-        const {category} = this.props;
-        const items = applyFilters({
-          key: 'Filter',
-          path: 'items__sales_items',
-          params: {
-              base_sales_cat:category
-          },
-          then:{
-            key:'Reject', 
-              params:{
-                active:false
-              }
-            }
-      })
-  
-      return items.map((d,v)=>{
-          console.log(d)
-         return( <div>
-             <img src={Table}/>
-             {d.name}
-             from EGP{this.getPrice(d.id)}
-          </div>
-         )
-      })
-        
+  renderItems = () => {
+    const { category } = this.props;
+    const items = applyFilters({
+      key: "Filter",
+      path: "items__sales_items",
+      params: {
+        base_sales_cat: category
+      },
+      then: {
+        key: "Reject",
+        params: {
+          active: false
+        }
       }
-      
-      getPrice(item){
-        const list = applyFilters({
-            key: 'List',
-            path: "items__prices",
-            select: {
-                sales_item:item
-            }
-          })
-          
-         const price= []
-         list.map(d=>price.push(d.price))
+    });
 
-      return min(price)
-    }
+    return items.map((d, v) => {
+      console.log(d);
+      return (
+        <div className={classes.item}>
+          <img src={Table} className={classes.image} />
+          <div className={classes.title}>{d.name}</div>
+          <div className={classes.price}>from EGP{this.getPrice(d.id)}</div>
+        </div>
+      );
+    });
+  };
 
-    render() {
-        return (
-        this.props.category?<div>
-            {this.renderItems()}
-           </div>
-           :<></>
-        )
-    }
+  getPrice(item) {
+    const list = applyFilters({
+      key: "List",
+      path: "items__prices",
+      select: {
+        sales_item: item
+      }
+    });
+
+    const price = [];
+    list.map(d => price.push(d.price));
+
+    return min(price);
+  }
+
+  render() {
+    return this.props.category ? (
+      <div className={classes.container}>{this.renderItems()}</div>
+    ) : (
+      <></>
+    );
+  }
 }
 const mapStateToProps = state => ({
-    category: get(state.items__base_sales_cat,'active',undefined)
-})
-    
-export default connect(mapStateToProps,mapDispatchToProps)(MainItems);
+  category: get(state.items__base_sales_cat, "active", undefined)
+});
 
+export default connect(mapStateToProps, mapDispatchToProps)(MainItems);
